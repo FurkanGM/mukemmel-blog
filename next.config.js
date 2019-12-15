c7516ace7b9284b31cd87fe2992327dc5eda8b97
furkangm <furkangezek2014@gmail.com>
2019-12-15 17:32:38 +0300

    update site adress

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withSass = require('@zeit/next-sass');
 const withCSS = require("@zeit/next-css");
 module.exports = withSass(withCSS({
   env: {
-    baseUrl: process.env.NODE_ENV === "development" ? 'http://localhost:3000' : 'https://personel-blog.herokuapp.com'
+    baseUrl: process.env.NODE_ENV === "development" ? 'http://localhost:3000' : 'http://www.furkangezek.com.tr'
   },
   modules: [
     '@nuxtjs/axios',
